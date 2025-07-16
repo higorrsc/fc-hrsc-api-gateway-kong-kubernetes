6a0afc5fecf7aa7efb8f015721f93117dcbd50e6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-07-16 20:10:18 -0300

    feat: :ambulance: add authentication with keycloak

---
+++ infra/kong-k8s/misc/token/get-token.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# kubectl exec -it testcurl -- sh
+
+# curl --location --request POST 'http://keycloak.iam/realms/bets/protocol/openid-connect/token' \
+# --header 'Content-Type: application/x-www-form-urlencoded' \
+# --data-urlencode 'client_id=kong' \
+# --data-urlencode 'grant_type=password' \
+# --data-urlencode 'username=maria' \
+# --data-urlencode 'password=maria' \
+# --data-urlencode 'client_secret=ucIBV9uUewtEnMit59Er9xODGQm2xEc1' \
+# --data-urlencode 'scope=openid'
+kubectl exec testcurl -- \
+  curl --location --request POST 'http://keycloak.iam/realms/bets/protocol/openid-connect/token' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data-urlencode 'client_id=kong' \
+  --data-urlencode 'grant_type=password' \
+  --data-urlencode 'username=maria' \
+  --data-urlencode 'password=maria' \
+  --data-urlencode 'client_secret=ucIBV9uUewtEnMit59Er9xODGQm2xEc1' \
+  --data-urlencode 'scope=openid'
