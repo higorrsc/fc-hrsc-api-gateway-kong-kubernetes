9904672cdb5aef2aa189081cfa3ee355a4716777
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-07-15 21:22:50 -0300

    feat: ✨ add api and kong ingress manifests

--- infra/kong-k8s/misc/apis/apis.sh
+++ infra/kong-k8s/misc/apis/apis.sh
@@ -2,3 +2,5 @@
 kubectl get ns bets || kubectl create ns bets
 kubectl apply -f kratelimit.yaml -n bets
 kubectl apply -f kprometheus.yaml
+kubectl apply -f bets-api.yaml -n bets
+kubectl apply -f king.yaml -n bets
