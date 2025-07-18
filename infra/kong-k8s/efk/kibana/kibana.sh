f968b289564d9cefa161a4a7f8093a3eb14f0901
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-07-17 21:07:11 -0300

    feat: ✨ define kibana resources

--- infra/kong-k8s/efk/kibana/kibana.sh
+++ infra/kong-k8s/efk/kibana/kibana.sh
@@ -4,4 +4,8 @@ helm install kibana elastic/kibana \
   --version=7.17.1 \
   --namespace=logs \
   --set service.type=NodePort \
-  --set service.nodePort=31000
+  --set service.nodePort=31000 \
+  --set resources.requests.cpu=200m \
+  --set resources.requests.memory=1Gi \
+  --set resources.limits.cpu=500m \
+  --set resources.limits.memory=1.5Gi
